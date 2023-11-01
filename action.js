c3db32aa579e73ed769ec892d8bcafa0f476beac
다온 <85875857+daon1213@users.noreply.github.com>
2023-11-01 18:38:02 +0900

    변경된 내용
    DOM 객체 생성

--- action.js
+++ action.js
@@ -1,41 +1,63 @@
-let projectId = 0;
+let projectIdCounter = 0; // 프로젝트 ID 카운터
 
-function deleteProject(id) {
-  const div = document.getElementById(id);
-  div.remove();
+function deleteProject(projectId) {
+  const projectElement = document.getElementById(projectId);
+  projectElement.remove();
 }
 
-function addTextBox() {
-  const input1Value = document.getElementById("input1").value;
-  const input2Value = document.getElementById("input2").value;
+// 새로운 프로젝트 추가 함수
+function addProject() {
+  const projectNameInputValue = document.getElementById("input1").value;
+  const projectDurationInputValue = document.getElementById("input2").value;
 
-  if (input1Value === "") {
+  if (projectNameInputValue === "") {
     alert("프로젝트 이름을 입력해주세요!");
     document.getElementById("input1").focus();
     return;
   }
-  if (input2Value === "") {
+  if (projectDurationInputValue === "") {
     alert("기간을 입력해주세요!");
     document.getElementById("input2").focus();
     return;
   }
 
-  projectId++;
-  const pId = projectId.toString();
+  projectIdCounter++;
+  const projectId = "project" + projectIdCounter;
 
-  const newTextDiv = document.createElement("div");
-  newTextDiv.className = "text-box";
-  newTextDiv.id = pId;
-  newTextDiv.innerHTML = `
-    <p class="text">${input1Value}</p>
-    <p class="year-text">(${input2Value})</p>
-    <button class='delete'onClick="deleteProject('${pId}')">삭제</button>
-  `;
+  // 새로운 프로젝트를 나타내는 DOM 객체를 생성
+  const newProjectDiv = document.createElement("div");
+  newProjectDiv.className = "text-box";
+  newProjectDiv.id = projectId;
 
+  const textElement = document.createElement("p");
+  textElement.className = "text";
+  textElement.textContent = projectNameInputValue;
+
+  const yearTextElement = document.createElement("p");
+  yearTextElement.className = "year-text";
+  yearTextElement.textContent = `(${projectDurationInputValue})`;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "delete";
+  deleteButton.textContent = "삭제";
+  deleteButton.addEventListener("click", () => {
+    deleteProject(projectId);
+  });
+
+  newProjectDiv.appendChild(textElement);
+  newProjectDiv.appendChild(yearTextElement);
+  newProjectDiv.appendChild(deleteButton);
+
+  // 새로운 프로젝트 DOM 객체를 컨테이너에 추가
   const container = document.getElementById("container");
-  container.appendChild(newTextDiv);
-  console.log(`프로젝트 : ${input1Value}, 기간 : ${input2Value}`);
+  container.appendChild(newProjectDiv);
 
+  // 콘솔에 프로젝트 정보 출력
+  console.log(
+    `프로젝트 : ${projectNameInputValue}, 기간 : ${projectDurationInputValue}`
+  );
+
+  // 입력 필드 초기화
   document.getElementById("input1").value = "";
   document.getElementById("input2").value = "";
 }
