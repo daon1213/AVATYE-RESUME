a932d5aad8aa492410bc6d7fb2d821b901cc6470
다온 <85875857+daon1213@users.noreply.github.com>
2023-11-03 16:54:46 +0900

    삭제 버튼 누를 시 로그 제거 기능 구현
    추가 버튼을 누를 시 기본 로그와 JSON 로그가 콘솔에 찍히는데 삭제 버튼을 누를 경우 각 로그를 제거

--- action.js
+++ action.js
@@ -1,45 +1,58 @@
-let projectIdCounter = 0; // 프로젝트 ID 카운터
+// 프로젝트 ID 카운터
+let projectIdCounter = 0;
 
+// 프로젝트 삭제 함수
 function deleteProject(projectId) {
   const projectElement = document.getElementById(projectId);
   projectElement.remove();
+
+  // 프로젝트 정보 로그 제거
+  console.clear();
 }
 
 // 새로운 프로젝트 추가 함수
 function addProject() {
-  const projectNameInputValue = document.getElementById("input1").value;
-  const projectDurationInputValue = document.getElementById("input2").value;
+  const projectNameInput = document.getElementById("input1");
+  const projectDurationInput = document.getElementById("input2");
 
-  if (projectNameInputValue === "") {
+  const projectName = projectNameInput.value;
+  const projectDuration = projectDurationInput.value;
+
+  if (projectName === "") {
     alert("프로젝트 이름을 입력해주세요!");
-    document.getElementById("input1").focus();
+    projectNameInput.focus();
     return;
   }
-  if (projectDurationInputValue === "") {
+  if (projectDuration === "") {
     alert("기간을 입력해주세요!");
-    document.getElementById("input2").focus();
+    projectDurationInput.focus();
     return;
   }
 
   projectIdCounter++;
   const projectId = "project" + projectIdCounter;
 
-  // 새로운 프로젝트를 나타내는 DOM 객체를 생성
+  // 새로운 프로젝트 요소 생성
   const newProjectDiv = document.createElement("div");
   newProjectDiv.className = "text-box";
   newProjectDiv.id = projectId;
 
+  // 프로젝트 이름 텍스트 엘리먼트 생성
   const textElement = document.createElement("p");
   textElement.className = "text";
-  textElement.textContent = projectNameInputValue;
+  textElement.textContent = projectName;
 
+  // 프로젝트 기간 텍스트 엘리먼트 생성
   const yearTextElement = document.createElement("p");
   yearTextElement.className = "year-text";
-  yearTextElement.textContent = `(${projectDurationInputValue})`;
+  yearTextElement.textContent = `(${projectDuration})`;
 
+  // 삭제 버튼 생성
   const deleteButton = document.createElement("button");
   deleteButton.className = "delete";
   deleteButton.textContent = "삭제";
+
+  // 삭제 버튼 클릭 이벤트 핸들러 등록
   deleteButton.addEventListener("click", () => {
     deleteProject(projectId);
   });
@@ -48,16 +61,21 @@ function addProject() {
   newProjectDiv.appendChild(yearTextElement);
   newProjectDiv.appendChild(deleteButton);
 
-  // 새로운 프로젝트 DOM 객체를 컨테이너에 추가
+  // 프로젝트 컨테이너에 추가
   const container = document.getElementById("container");
   container.appendChild(newProjectDiv);
 
   // 콘솔에 프로젝트 정보 출력
-  console.log(
-    `프로젝트 : ${projectNameInputValue}, 기간 : ${projectDurationInputValue}`
-  );
+  console.log(`프로젝트 : ${projectName}, 기간 : ${projectDuration}`);
+
+  // JSON으로 프로젝트 정보 출력
+  const projectInfo = {
+    projectName: projectName,
+    projectDuration: projectDuration,
+  };
+  console.log("프로젝트 정보: " + JSON.stringify(projectInfo));
 
   // 입력 필드 초기화
-  document.getElementById("input1").value = "";
-  document.getElementById("input2").value = "";
+  projectNameInput.value = "";
+  projectDurationInput.value = "";
 }
